69d4a5515d1d992254eeb8f8760269bcbc298db9
ThDelgado <100494591+ThDelgado@users.noreply.github.com>
2024-08-14 22:02:42 -0400

    creacion de archivo

---
+++ calculoAritmetico.py
@@ -0,0 +1,15 @@
+
+
+#creando variables
+
+a = 20
+b = 30
+
+# operacion aritmetica
+sum = (a + b)
+
+# imprimiendo resultado
+print("La suma es: " ,sum)
+
+
+#Thelma Delgado operacion aritmetica ejercicio comprobacion m3s3
